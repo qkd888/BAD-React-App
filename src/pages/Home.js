30432482bab4b37a607468b3b6f3e308b3cb3547
Quintin Kelly Dekker <quintinkellydekker888@gmail.com>
2025-05-13 12:17:09 +0200

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,8 +3,8 @@ import React from 'react';
 function Home() {
   return (
     <div className="page">
-      <h1>Bosapie Designs</h1>
-      <p id="home-p">Bosapie Designs is dedicated to crafting exceptional, high-quality design solutions that make a lasting impact.  We are driven by a passion for creativity and a commitment to meticulous attention to detail. Our mission is to inspire, engage, and elevate our clients' brands through innovative and visually stunning designs that resonate with their target audience and drive meaningful results.</p>
+      <h1>QKD888 Designs</h1>
+      <p id="home-p">QKD888 Designs is dedicated to crafting exceptional, high-quality design solutions that make a lasting impact.  We are driven by a passion for creativity and a commitment to meticulous attention to detail. Our mission is to inspire, engage, and elevate our clients' brands through innovative and visually stunning designs that resonate with their target audience and drive meaningful results.</p>
     </div>
   );
 }
